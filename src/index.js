4be75dbdb686866180e1cb6d82c37e724c13ab5d
Luke G <lukegronert@gmail.com>
2021-07-19 18:59:17 +0800

    Removed HashRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom';
 import './styles/index.css';
 import Routes from './components/Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
       <Routes />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
