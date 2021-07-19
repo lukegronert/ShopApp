8a6a6884f5dc383bcac1fe92e8871c99f21cddfb
Luke G <lukegronert@gmail.com>
2021-07-19 20:28:06 +0800

    Added comments in Routes.js saying that we must use HashRouter because BrowserRouter does not work with gh-pages

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
+// Use HashRouter because BrowserRouter does not work with gh-pages
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
 import Shop from './Shop';
 
 export default function Routes() {
     return (
+        // Must include basename in Router when using HashRouter
         <Router basename='/'>
             <Switch>
                 <Route exact path='/' component={App} />
