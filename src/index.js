2f23110629924d71d32a95ce10100cb917b801fe
Luke G <lukegronert@gmail.com>
2021-07-19 18:54:16 +0800

    Added {} around HashRouter in index.js import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HashRouter from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './styles/index.css';
 import Routes from './components/Routes';
 
